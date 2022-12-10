57e2c8d28d3c388d7294335e169182fcf19aec93
0xCitaDel <allashuvalova@MacBook-Air-Alla.local>
2022-12-10 15:05:50 +0300

    list model

--- LinkedList.c
+++ LinkedList.c
@@ -9,6 +9,11 @@ struct Node {
 
 
 int main() {
-    struct Node *list = NULL;
+    Node *list = NULL;
+    Node a = {10}, b = {33}, c = {25}, t = {34};
+    list = &a;
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
     return 0;
 }
