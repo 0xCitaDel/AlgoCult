9b40e316aa52b0b9eb300c093e0835afd191d6d5
0xCitaDel <allashuvalova@MacBook-Air-Alla.local>
2022-12-10 16:31:29 +0300

    push tests with array for

--- LinkedList.c
+++ LinkedList.c
@@ -9,16 +9,17 @@ struct Node {
 };
 
 void printList(Node *list);
+
 void pushList(Node **plist, Data d);
 
-void printList(Node *list){
+void printList(Node *list) {
     for (Node *p = list; p != NULL; p = p->next) {
         printf("%d ", p->data);
     }
     printf("\n");
 }
 
-void pushList(Node **plist, Data d){
+void pushList(Node **plist, Data d) {
     Node *p = malloc(sizeof(Node));
     p->data = d;
     p->next = *plist;
@@ -26,16 +27,12 @@ void pushList(Node **plist, Data d){
 }
 
 int main() {
+    Data test[] = {21, 5, 77, 44, 6};
     Node *list = NULL;
-    Node a = {10}, b = {33}, c = {25};
-    list = &a;
-    a.next = &b;
-    b.next = &c;
-    c.next = NULL;
-
-    printList(list);
-    pushList(&list, 10);
-    printList(list);
+    for (size_t i = 0; i < sizeof(test) / sizeof(test[0]); i++) {
+        pushList(&list, test[i]);
+        printList(list);
+    }
 
     return 0;
 }
