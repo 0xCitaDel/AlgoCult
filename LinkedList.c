604e6a8d536c90be51350a3e2f1594a6e923d8d1
0xCitaDel <allashuvalova@MacBook-Air-Alla.local>
2022-12-10 16:23:04 +0300

    push add function

--- LinkedList.c
+++ LinkedList.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef int Data;
 typedef struct Node Node;
@@ -7,6 +8,9 @@ struct Node {
     Node *next;
 };
 
+void printList(Node *list);
+void pushList(Node **plist, Data d);
+
 void printList(Node *list){
     for (Node *p = list; p != NULL; p = p->next) {
         printf("%d ", p->data);
@@ -14,14 +18,23 @@ void printList(Node *list){
     printf("\n");
 }
 
+void pushList(Node **plist, Data d){
+    Node *p = malloc(sizeof(Node));
+    p->data = d;
+    p->next = *plist;
+    *plist = p;
+}
+
 int main() {
     Node *list = NULL;
-    Node a = {10}, b = {33}, c = {25}, t = {34};
+    Node a = {10}, b = {33}, c = {25};
     list = &a;
     a.next = &b;
     b.next = &c;
     c.next = NULL;
 
+    printList(list);
+    pushList(&list, 10);
     printList(list);
 
     return 0;
